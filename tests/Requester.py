b5eef11a7fb4101574f96621a85817ca47fbcc35
Enrico Minack <github@enrico.minack.dev>
2022-01-10 17:53:05 +0100

    Remove Framework setter as those have side-effects on other tests

--- tests/Requester.py
+++ tests/Requester.py
@@ -31,11 +31,9 @@ REPO_NAME = "PyGithub/PyGithub"
 
 
 class RequesterUnThrottled(Framework.TestCase):
-    def setUp(self):
-        Framework.setSecondsBetweenRequests(None)
-        Framework.setSecondsBetweenWrites(None)
-        Framework.setPerPage(10)
-        super().setUp()
+    seconds_between_requests = None
+    seconds_between_writes = None
+    per_page = 10
 
     def testShouldNotDeferRequests(self):
         with mock.patch("github.Requester.time.sleep") as sleep_mock:
@@ -47,11 +45,9 @@ class RequesterUnThrottled(Framework.TestCase):
 
 
 class RequesterThrottled(Framework.TestCase):
-    def setUp(self):
-        Framework.setSecondsBetweenRequests(1.0)
-        Framework.setSecondsBetweenWrites(3.0)
-        Framework.setPerPage(10)
-        super().setUp()
+    seconds_between_requests = 1.0
+    seconds_between_writes = 3.0
+    per_page = 10
 
     def testShouldDeferRequests(self):
         now = [datetime.datetime.utcnow()]
